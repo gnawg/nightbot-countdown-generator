1735ad78a88179985d56befaae7070535f433087
gnawg <gnawgy.gnawgy@gmail.com>
2022-09-22 11:30:23 -0400

    draft 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { DateTime } from "luxon";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const inputTime = DateTime.fromISO("2022-10-22T17:00:00", { zone: "PST8PDT" });
---
+++ src/App.tsx
@@ -0,0 +1,182 @@
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import {
+  AppBar,
+  Autocomplete,
+  Box,
+  Container,
+  Grid,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { green, purple } from "@mui/material/colors";
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+import React, { useEffect, useState } from "react";
+import {
+  DateTime,
+  Zone,
+  SystemZone,
+  Duration,
+  Settings,
+  Interval,
+} from "luxon";
+import { Timezones } from "./constants";
+
+const theme = createTheme({
+  /*   palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  }, */
+});
+
+const units = ["years", "months", "days", "hours", "minutes", "seconds"];
+function singular(str) {
+  return str.slice(0, -1);
+}
+function DatetimeToCountdown(dt: DateTime): String {
+  const dur = dt.diffNow(units);
+  const ago = dur.valueOf() < 0;
+  console.log(dur);
+
+  const str = units.reduce(
+    (str, unit) =>
+      dur[unit]
+        ? `${str} ${Math.abs(Math.round(dur[unit]))} ${
+            dur[unit] === 1 ? singular(unit) : unit
+          }`
+        : str,
+    ""
+  );
+
+  return `${str}${ago ? " ago" : ""}`;
+}
+
+function App() {
+  const [targetTimezone, setTargetTimezone] = useState(
+    localStorage.timezone || new SystemZone().name
+  );
+  const [targetTime, setTargetTime] = useState(DateTime.now());
+  console.log(targetTime.zoneName);
+  useEffect(() => {
+    localStorage.timezone = targetTimezone;
+    Settings.defaultZone = targetTimezone;
+  }, [targetTimezone]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterLuxon}>
+        <AppBar position="static">
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <Typography variant="h6" component="h1">
+              Nightbot Countdown Tool
+            </Typography>
+            <title>Nightbot Countdown Tool</title>
+          </Toolbar>
+        </AppBar>
+        {/* {targetTimezone}
+        {targetTime} */}
+        {DatetimeToCountdown(targetTime)}
+        <Container component="main" maxWidth="sm" sx={{ mt: 2 }}>
+          <Grid container spacing={3}>
+            {/* <Grid item xs={12} sm={6}>
+              <Autocomplete
+                disablePortal
+                id="timezone-select"
+                options={Timezones}
+                defaultValue={targetTimezone}
+                renderInput={(params) => {
+                  return <TextField {...params} label="Timezone" />;
+                }}
+              />
+            </Grid> */}
+            <Grid item xs={12} sm={6}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="DateTimePicker"
+                value={targetTime}
+                onChange={(newValue) => {
+                  setTargetTime(newValue);
+                }}
+              />
+            </Grid>
+            {/*  */}
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="script"
+                name="script"
+                fullWidth
+                variant="standard"
+                //value={value.toNow()}
+              />
+            </Grid>
+            {/* etc */}
+            {/*
+            <Grid item xs={12}>
+              <TextField
+                id="address2"
+                name="address2"
+                label="Address line 2"
+                fullWidth
+                autoComplete="shipping address-line2"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="city"
+                name="city"
+                label="City"
+                fullWidth
+                autoComplete="shipping address-level2"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="state"
+                name="state"
+                label="State/Province/Region"
+                fullWidth
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="zip"
+                name="zip"
+                label="Zip / Postal code"
+                fullWidth
+                autoComplete="shipping postal-code"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="country"
+                name="country"
+                label="Country"
+                fullWidth
+                autoComplete="shipping country"
+                variant="standard"
+              />
+            </Grid> */}
+          </Grid>
+        </Container>
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
